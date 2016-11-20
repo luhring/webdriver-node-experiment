2c471f59055e7c286170485f16a1519037fddeea
Dan Luhring <dan+github@luhrings.com>
2016-11-20 18:30:03 -0500

    Initial commit

---
+++ google_search.js
@@ -0,0 +1,20 @@
+// configuration
+const _seleniumBrowser = "chrome";
+const _seleniumRemoteHost = "selenium";
+const _seleniumRemotePort = 4444
+const _seleniumRemoteServerUri = "http://" + _seleniumRemoteHost + ":" + _seleniumRemotePort + "/wd/hub";
+
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser(_seleniumBrowser)
+    .usingServer(_seleniumRemoteServerUri)
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
