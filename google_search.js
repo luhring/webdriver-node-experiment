61369ad30c5210b6edee25a3655ce29ebf87e857
Dan Luhring <dan+github@luhrings.com>
2016-11-20 19:23:07 -0500

    Insert sample code for taking a screenshot

--- google_search.js
+++ google_search.js
@@ -17,4 +17,13 @@ driver.get('http://www.google.com/ncr');
 driver.findElement(By.name('q')).sendKeys('webdriver');
 driver.findElement(By.name('btnG')).click();
 driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+
+driver.takeScreenshot().then(
+    function(image, err) {
+        require('fs').writeFile('out.png', image, 'base64', function(err) {
+            console.log(err);
+        });
+    }
+);
+
 driver.quit();
